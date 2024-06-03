23366ed4f0423037c4d51c5901f1a056178de4ac
Sümeyra Polat <180315055@ogr.cbu.edu.tr>
2024-06-04 01:01:09 +0300

    Firebase gradles are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -66,4 +67,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Firebase
+    implementation(libs.firebase.bom)
+    implementation("com.google.firebase:firebase-analytics")
 }
