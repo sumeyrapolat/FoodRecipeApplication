7ed4b6830ac923658bb7282adc8b931293b25a4c
Sümeyra Polat <180315055@ogr.cbu.edu.tr>
2024-06-06 16:08:18 +0300

    Main screen RecipeCard is written

--- app/src/main/java/com/example/foodrecipeapplicaiton/view/components/RecipeCard.kt
+++ app/src/main/java/com/example/foodrecipeapplicaiton/view/components/RecipeCard.kt
@@ -1,2 +1,84 @@
 package com.example.foodrecipeapplicaiton.view.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.foodrecipeapplicaiton.R
+
+@Composable
+fun RecipeCard(
+    title: String,
+    ingredients: String,
+    imageRes: Int
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .clip(RoundedCornerShape(16.dp)),
+        //elevation = 4.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .background(Color(0xFFF5F5F5))
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(27.dp)),
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = title,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = ingredients,
+                fontSize = 16.sp,
+                color = Color.Gray,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RecipeCardPreview() {
+    RecipeCard(
+        title = "Delicious Pasta",
+        ingredients = "Tomatoes, Basil, Garlic, Olive oil, Pasta, Salt, Pepper, Parmesan",
+        imageRes = R.drawable.ic_launcher_background
+    )
+}
