db04b45cea71c0e80088885890e30c5322a3fd62
Sümeyra Polat <180315055@ogr.cbu.edu.tr>
2024-06-08 23:30:41 +0300

    A profile card was designed for the profile page and a logout button was defined for logout, and its function was defined.

--- app/src/main/java/com/example/foodrecipeapplicaiton/view/screens/ProfileCard.kt
+++ app/src/main/java/com/example/foodrecipeapplicaiton/view/screens/ProfileCard.kt
@@ -1,2 +1,75 @@
 package com.example.foodrecipeapplicaiton.view.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+@Composable
+fun ProfileCard(userEmail: String, username: String, onLogout: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(25.dp))
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "Merhaba : $username",
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    color = Color.Black,
+                    maxLines = 1,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = userEmail,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    color = Color.Black,
+                    maxLines = 1,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Button(
+                    modifier = Modifier.padding(5.dp),
+                    onClick = { onLogout() },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Black)
+
+                ) {
+                    Text(
+                        text = "Logout",
+                        color = Color.White,
+                        fontSize = 20.sp,
+                    )
+                }
+            }
+        }
+    }
+}
