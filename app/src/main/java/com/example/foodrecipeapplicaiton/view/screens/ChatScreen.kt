523f28c1e46d5da2aba936a89e681a819fe78762
Sümeyra Polat <180315055@ogr.cbu.edu.tr>
2024-06-08 08:29:00 +0300

    Added ChatScreen composable
    -Created the ChatScreen composable for displaying the chat feature.
    -Integrated the ChatScreen composable into the navigation flow

---
+++ app/src/main/java/com/example/foodrecipeapplicaiton/view/screens/ChatScreen.kt
@@ -0,0 +1,11 @@
+package com.example.foodrecipeapplicaiton.view.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ChatScreen(navController: NavController) {
+
+    Text(text = "Hello Chat Screen")
+}
